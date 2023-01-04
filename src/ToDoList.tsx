5d5ff04c4de6a128a32be62cd5f97e43ee2db234
노정은 <wjddms4107@naver.com>
2023-01-04 22:07:29 +0900

    [Refactoring] selector로 category 분류하기

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,36 +1,28 @@
 import React from "react";
-import { useRecoilValue } from "recoil";
-import { toDoSelector, toDoState } from "./components/atoms";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { categoryState, toDoSelector } from "./components/atoms";
 import CreateToDo from "./components/CreateToDo";
 import ToDo from "./components/ToDo";
 
 function ToDoList() {
-  const [toDo, doing, done] = useRecoilValue(toDoSelector);
+  const toDos = useRecoilValue(toDoSelector);
+  const [category, setCategory] = useRecoilState(categoryState);
+  const onInput = (event: React.FormEvent<HTMLSelectElement>) => {
+    setCategory(event.currentTarget.value);
+  };
 
   return (
     <div>
       <h1>ToDos</h1>
       <hr />
+      <select value={category} onInput={onInput}>
+        <option value="TO_DO">To Do</option>
+        <option value="DOING">Doing</option>
+        <option value="DONE">Done</option>
+      </select>
       <CreateToDo />
-      <h2>To Do</h2>
       <ul>
-        {toDo.map((toDo) => (
-          // eslint-disable-next-line react/jsx-props-no-spreading
-          <ToDo key={toDo.id} {...toDo} />
-        ))}
-      </ul>
-      <hr />
-      <h2>Doing</h2>
-      <ul>
-        {doing.map((toDo) => (
-          // eslint-disable-next-line react/jsx-props-no-spreading
-          <ToDo key={toDo.id} {...toDo} />
-        ))}
-      </ul>
-      <hr />
-      <h2>Done</h2>
-      <ul>
-        {done.map((toDo) => (
+        {toDos?.map((toDo) => (
           // eslint-disable-next-line react/jsx-props-no-spreading
           <ToDo key={toDo.id} {...toDo} />
         ))}
