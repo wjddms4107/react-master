2cc83f8ba0729ecabad237132fd8eb795b09b311
노정은 <wjddms4107@naver.com>
2023-01-04 22:05:03 +0900

    [Feat] 각 카테고리별로 보여주기 - selector 사용

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,31 +1,41 @@
-/* eslint-disable react/jsx-props-no-spreading */
-import React, { useState } from "react";
-import { useForm } from "react-hook-form";
-
-interface IForm {
-  toDo: string;
-}
+import React from "react";
+import { useRecoilValue } from "recoil";
+import { toDoSelector, toDoState } from "./components/atoms";
+import CreateToDo from "./components/CreateToDo";
+import ToDo from "./components/ToDo";
 
 function ToDoList() {
-  const { register, handleSubmit, setValue } = useForm<IForm>();
-
-  const handleValid = (data: IForm) => {
-    setValue("toDo", "");
-  };
+  const [toDo, doing, done] = useRecoilValue(toDoSelector);
 
   return (
     <div>
       <h1>ToDos</h1>
       <hr />
-      <form onSubmit={handleSubmit(handleValid)}>
-        <input
-          {...register("toDo", {
-            required: "Please write a To Do",
-          })}
-          placeholder="Write a to do"
-        />
-        <button type="submit">Add</button>
-      </form>
+      <CreateToDo />
+      <h2>To Do</h2>
+      <ul>
+        {toDo.map((toDo) => (
+          // eslint-disable-next-line react/jsx-props-no-spreading
+          <ToDo key={toDo.id} {...toDo} />
+        ))}
+      </ul>
+      <hr />
+      <h2>Doing</h2>
+      <ul>
+        {doing.map((toDo) => (
+          // eslint-disable-next-line react/jsx-props-no-spreading
+          <ToDo key={toDo.id} {...toDo} />
+        ))}
+      </ul>
+      <hr />
+      <h2>Done</h2>
+      <ul>
+        {done.map((toDo) => (
+          // eslint-disable-next-line react/jsx-props-no-spreading
+          <ToDo key={toDo.id} {...toDo} />
+        ))}
+      </ul>
+      <hr />
     </div>
   );
 }
---
+++ src/components/ToDo.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { useSetRecoilState } from "recoil";
+import { IToDo, toDoState } from "./atoms";
+
+function ToDo({ text, category, id }: IToDo) {
+  const setToDos = useSetRecoilState(toDoState);
+
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const {
+      currentTarget: { name },
+    } = event;
+
+    setToDos((oldToDos) => {
+      const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
+      const newToDo = { text, id, category: name as any };
+      return [
+        ...oldToDos.slice(0, targetIndex),
+        newToDo,
+        ...oldToDos.slice(targetIndex + 1),
+      ];
+    });
+  };
+
+  return (
+    <li>
+      <span>{text}</span>
+      {category !== "DOING" && (
+        <button name="DOING" onClick={onClick} type="button">
+          Doing
+        </button>
+      )}
+      {category !== "TO_DO" && (
+        <button name="TO_DO" onClick={onClick} type="button">
+          To Do
+        </button>
+      )}
+      {category !== "DONE" && (
+        <button name="DONE" onClick={onClick} type="button">
+          Done
+        </button>
+      )}
+    </li>
+  );
+}
+
+export default ToDo;
