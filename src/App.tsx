7fe05c4bb9830688df1b77c2b967b6a4cdc05acc
노정은 <wjddms4107@naver.com>
2023-01-07 22:41:41 +0900

    Variants

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,44 @@ import React from "react";
 import styled from "styled-components";
 import { motion } from "framer-motion";
 
+const boxVariants = {
+  start: {
+    opacity: 0,
+    scale: 0.5,
+  },
+  end: {
+    scale: 1,
+    opacity: 1,
+    transition: {
+      type: "spring",
+      duration: 0.5,
+      bounce: 0.5,
+      delayChildren: 0.5, // 자식들을 기다리게 한다.
+      staggerChildren: 0.3, // 자식들에게 시간차로 딜레이를 주는 것!! 이거만 해주면 어떠한 계산도 안해줘도 돼 ㅎㅎ
+    },
+  },
+};
+
+const circleVariants = {
+  start: {
+    opacity: 0,
+    y: 10,
+  },
+  end: {
+    opacity: 1,
+    y: 0,
+  },
+};
+
 function App() {
   return (
     <Wrapper>
-      <Box
-        transition={{ type: "spring", delay: 1 }}
-        initial={{ scale: 0 }}
-        animate={{ scale: 1, rotateZ: 360 }}
-      />
+      <Box variants={boxVariants} initial="start" animate="end">
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+      </Box>
     </Wrapper>
   );
 }
@@ -25,8 +55,19 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 40px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const Circle = styled(motion.div)`
   background-color: white;
-  border-radius: 15px;
+  height: 70px;
+  width: 70px;
+  place-self: center;
+  border-radius: 35px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
