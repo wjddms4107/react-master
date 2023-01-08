5a00636eb3017bbe42f29d8f7c1d912a86a4537a
노정은 <wjddms4107@naver.com>
2023-01-08 20:31:02 +0900

    초기세팅

--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,14 @@
 import { DefaultTheme } from "styled-components";
 
-export const darkTheme: DefaultTheme = {
-  bgColor: "#2f3640",
-  textColor: "white",
-  accentColor: "#4cd137",
+export const theme: DefaultTheme = {
+  red: "#E51013",
+  black: {
+    veryDark: "#141414",
+    darker: "#181818",
+    lighter: "#2F2F2F",
+  },
+  white: {
+    lighter: "#fff",
+    darker: "#e5e5e5",
+  },
 };
